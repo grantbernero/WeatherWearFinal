93b8c15ceaf4b64bce870f37f27391d09a8e4029
GBernero <student@gbernero.local>
2017-04-27 10:38:25 -0500

    api

--- WeatherWare/ViewController.swift
+++ WeatherWare/ViewController.swift
@@ -10,12 +10,31 @@ import UIKit
 
 class ViewController: UIViewController
 {
-
+    var weather = [[String: String]]()
     override func viewDidLoad()
     {
         super.viewDidLoad()
+        
+        let urlString = "https://api.darksky.net/forecast/073a234b57601d6861b0f5e0d44defe4/42.15704,-88.14812"
+        
+        if let url = NSURL(string: urlString)
+        {
+            if let myData = try? NSData(contentsOf: url as URL, options: [])
+            {
+                let json = JSON(data:myData as Data)
+                print("ok to parse")
+                parse(json: json)
+            }
+        }
+  
     }
-
-
+    
+    func parse(json: JSON)
+    {
+        let result = json["currently"].dictionaryValue
+        let temperature = result["temperature"]!.stringValue
+        print(temperature)
+        
+    }
 }
 
